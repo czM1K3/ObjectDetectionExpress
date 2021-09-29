dd8787a3a81a928339725412f26072c5d475860d
czM1K3 <misamadera1@gmail.com>
2021-09-29 09:59:48 +0200

    Error handling

--- index.js
+++ index.js
@@ -21,23 +21,32 @@ const app = express();
 const port = 3000;
 
 app.post("/", upload.single("image"), async (req, res) => {
-	const buffer = req.file.buffer;
-	const result = await model.executeAsync(buffer);
-	const formated = Array(result[0].length);
-	for (let i = 0; i < formated.length; i++) {
-		formated[i] = {
-			type: labels[result[2][i]],
-			precision: result[1][i],
-			position: result[0][i].length > 2 ? {
-				x: result[0][i][0] + ((result[0][i][2] - result[0][i][0]) / 2),
-				y: result[0][i][1] + ((result[0][i][3] - result[0][i][1]) / 2),
-			} : {
-				x: result[0][i][0],
-				y: result[0][i][1],
-			},
+	try {
+
+		const buffer = req.file.buffer;
+		console.log(model);
+		const result = await model.executeAsync(buffer);
+		const formated = Array(result[0].length);
+		for (let i = 0; i < formated.length; i++) {
+			formated[i] = {
+				type: labels[result[2][i]],
+				precision: result[1][i],
+				position: result[0][i].length > 2 ? {
+					x: result[0][i][0] + ((result[0][i][2] - result[0][i][0]) / 2),
+					y: result[0][i][1] + ((result[0][i][3] - result[0][i][1]) / 2),
+				} : {
+					x: result[0][i][0],
+					y: result[0][i][1],
+				},
+			}
 		}
+		res.json(formated);
+	} catch (e) {
+		res.statusCode = 500;
+		res.write("Internal Server Error\n");
+		res.write(`Error: "${e.message}"`);
+		res.end();
 	}
-	res.json(formated);
 });
 
 app.listen(port, () => {
